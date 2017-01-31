a557ce2d9c593615fcfffc51819326ff5881b860
mkrivos <nezmar@tutok.sk>
2017-01-31 15:56:16 +0100

    initial commit

---
+++ src/loader.cpp
@@ -0,0 +1,23 @@
+///
+/// \package astlib
+/// \file loader.cpp
+///
+/// \author Marian Krivos <marian.krivos@rsys.sk>
+/// \date 31Jan.,2017 
+/// \brief definicia typu
+///
+/// (C) Copyright 2017 R-SYS s.r.o
+/// All rights reserved.
+/// See LICENCE file in project root directory
+///
+#include "astlib/CodecDeclarationLoader.h"
+
+
+using namespace astlib;
+
+int main(int argc, char* argv[])
+{
+    CodecDeclarationLoader loader;
+
+    return 0;
+}
