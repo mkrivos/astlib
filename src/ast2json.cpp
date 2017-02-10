eb3aafd678ffef6a99cb9aa341033b704eba34e7
mkrivos <nezmar@tutok.sk>
2017-02-10 11:31:31 +0100

    Remove redundant code

--- src/ast2json.cpp
+++ src/ast2json.cpp
@@ -252,47 +252,7 @@ protected:
                                 auto codec = _codecs[category];
                                 if (codec)
                                 {
-#if 1
-                                    unsigned char bytes[33+1+16+2+2+8+3+6+2+4+4+2+4+3+2+4+2+2+7+1+2+1+2] = {
-                                        48, // CAT
-                                        0, 1+2+3+2+3+1+4+2+2+3+6+17+16+2+4+4+2+4+3+2+4+2+2+7+1+2+1+2, // size
-                                        0xFF, 0xFF, 0xFF, 0xF8,// FSPEC
-
-                                        5, 6,  // 10 - sac sic
-                                        0, 0, 200, // 140 - time of day
-                                        0xfe, // 20 - Target Report Descriptor
-                                        0xFF, 0xFF,0xFF, 0xFF, // 40 - polar coords
-                                        0xFF, 0xFF, // 70 - mode 3A
-                                        0xFF, 0xFF, // 90 - mode C
-                                        0xFE, 0x88, 0x44,0x88, 0x44, 0x88, 0x44, 0x88, // 130 - plot characteristics
-
-                                        0xFF, 0xFF, 0xFF, // 220 - aircraft address
-                                        0x42, 0x55, 0x12, 0x45, 0x42, 0x24, // 240 - Aircraft Identification
-                                        2, 4,4,4,4,4,4,2,1,  5,5,5,5,5,5,2,1,  // item 250 Mode S Comm B data
-                                        1,0, // 161 - track number
-                                        0xFF, 0xFF, 0x00, 0x01, // 42 - cartes coords
-                                        0xFF, 0xFF, 0xFF, 0xFF, // 200
-                                        0xFF, 0xFE, // 170 - track status
-
-                                        0xFF, 0xFF, 0xFF, 0xFF, // 210 - track quality
-                                        3,5,6, // 30 - WE Condition
-                                        0xFF, 0xFE, // 80 - mode 3a conf
-                                        0xFF, 0xFF, 0xFF, 0xFF, // 100 - mode C conf
-                                        0xFF, 0xFF, // 110 - 3d height
-                                        0xC0, 0xFF,0xFF, 2, 0,1,0,2,0,3, 0,1,0,2,0,3, // 120 - radial dopler speed
-                                        0xFF, 0xFF, // 230 - Acas comm
-
-                                        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // 260 - ACAS resolution
-                                        0xFF, // 55
-                                        0xFF, 0xFF,// 50
-                                        0xFF, // 65
-                                        0xFF, 0xFF,// 60
-
-                                    };
-                                    _decoder.decode(*codec, decoderHandler, bytes, sizeof(bytes));
-#else
                                     _decoder.decode(*codec, decoderHandler, buffer, bytes);
-#endif
                                 }
                             }
                         }
