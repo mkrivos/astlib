ad52e32539ba6030cc968786afda315d8757d05e
Marian Krivos <nezmar@tutok.sk>
2017-02-26 09:39:44 +0100

    Version 1.0.0 Bump cmake to c++ 14, added Astlib.h with version definition

---
+++ astlib/Astlib.h
@@ -0,0 +1,25 @@
+///
+/// \package astlib
+/// \file Astlib.h
+///
+/// \author Marian Krivos <marian.krivos@rsys.sk>
+/// \date 31Jan.,2017 
+/// \brief library main entry
+///
+/// (C) Copyright 2017 R-SYS s.r.o
+/// All rights reserved.
+///
+
+#pragma once
+
+namespace astlib
+{
+
+#define ASTLIB_MAJOR 0x01
+#define ASTLIB_MINOR 0x00
+#define ASTLIB_PATCH 0x00
+
+#define ASTLIB_VERSION ((ASTLIB_MAJOR<<16) | ASTLIB_MINOR<<8 | ASTLIB_PATCH)
+
+} /* namespace astlib */
+
