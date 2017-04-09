c5ba5baf33ab5afa6d6d87f77afb914b8fed7df0
Marian Krivos <nezmar@tutok.sk>
2017-04-09 10:13:10 +0200

    generate asterixitems.js

--- astlib/bootstrap/generate_symbols.cpp
+++ astlib/bootstrap/generate_symbols.cpp
@@ -394,100 +394,127 @@ int main(int argc, char* argv[])
     {
         BitsRegister bits;
         auto files = bits.populateCodecsFromDirectory(specs);
-
-        Poco::FileOutputStream header(module + ".h");
-        header << "/// @brief file generated from XML asterix descriptions" << std::endl << std::endl;
-        header << "#pragma once" << std::endl << std::endl;
-        header << "#include \"astlib/AsterixItemCode.h\"" << std::endl << std::endl;
-        header << "namespace astlib {" << std::endl << std::endl;
-
-
-        for(auto file: files)
-        {
-            bits.load(file);
-        }
-
         auto& globals = bits.symbols;
 
-        int index = 1;
-        for (const auto& entry : globals)
         {
-            astlib::PrimitiveItem item = entry.second;
-            std::string upperName = Poco::toUpper(entry.first);
-            Poco::replaceInPlace(upperName, ".", "_");
+            Poco::FileOutputStream header(module + ".h");
+            header << "/// @brief file generated from XML asterix descriptions" << std::endl << std::endl;
+            header << "#pragma once" << std::endl << std::endl;
+            header << "#include \"astlib/AsterixItemCode.h\"" << std::endl << std::endl;
+            header << "namespace astlib {" << std::endl << std::endl;
 
-            header << "constexpr AsterixItemCode " << upperName << "_CODE(" << Poco::NumberFormatter::formatHex(index, 4, true) << ", PrimitiveType::" << item.getType().toString() << ", " << item.isArray() << ");  ///< " << item.getDescription() << std::endl;
-            index++;
+
+            for(auto file: files)
+            {
+                bits.load(file);
+            }
+
+            int index = 1;
+            for (const auto& entry : globals)
+            {
+                astlib::PrimitiveItem item = entry.second;
+                std::string upperName = Poco::toUpper(entry.first);
+                Poco::replaceInPlace(upperName, ".", "_");
+
+                header << "constexpr AsterixItemCode " << upperName << "_CODE(" << Poco::NumberFormatter::formatHex(index, 4, true) << ", PrimitiveType::" << item.getType().toString() << ", " << item.isArray() << ");  ///< " << item.getDescription() << std::endl;
+                index++;
+            }
+
+            header << std::endl;
+
+            for (const auto& entry : globals)
+            {
+                std::string upperName = Poco::toUpper(entry.first);
+                Poco::replaceInPlace(upperName, ".", "_");
+
+                header << "extern const std::string SYMBOL_" << upperName << ";" << std::endl;
+            }
+            header << std::endl << "constexpr int ASTERIX_ITEM_COUNT = " << Poco::NumberFormatter::format(globals.size()) << ";" << std::endl << std::endl;
+
+            header << "AsterixItemCode asterixSymbolToCode(const std::string& symbol);" << std::endl;
+            header << "const std::string& asterixCodeToSymbol(AsterixItemCode code);" << std::endl;
+            header << std::endl << "}" << std::endl;
         }
-
-        header << std::endl;
-
-        for (const auto& entry : globals)
-        {
-            std::string upperName = Poco::toUpper(entry.first);
-            Poco::replaceInPlace(upperName, ".", "_");
-
-            header << "extern const std::string SYMBOL_" << upperName << ";" << std::endl;
-        }
-        header << std::endl << "constexpr int ASTERIX_ITEM_COUNT = " << Poco::NumberFormatter::format(globals.size()) << ";" << std::endl << std::endl;
-
-        header << "AsterixItemCode asterixSymbolToCode(const std::string& symbol);" << std::endl;
-        header << "const std::string& asterixCodeToSymbol(AsterixItemCode code);" << std::endl;
-        header << std::endl << "}" << std::endl;
 
         // -------------------------------------------------------------------------------------------------------------
 
-        Poco::FileOutputStream source(module + ".cpp");
-        source << "/// @brief file generated from XML asterix descriptions" << std::endl << std::endl;
-        source << "#include \"" << module << ".h\"" << std::endl;
-        source << "#include <unordered_map>" << std::endl << std::endl;
-        source << "namespace astlib {" << std::endl << std::endl;
-
-        for (const auto& entry : globals)
         {
-            std::string upperName = Poco::toUpper(entry.first);
-            Poco::replaceInPlace(upperName, ".", "_");
+            Poco::FileOutputStream source(module + ".cpp");
+            source << "/// @brief file generated from XML asterix descriptions" << std::endl << std::endl;
+            source << "#include \"" << module << ".h\"" << std::endl;
+            source << "#include <unordered_map>" << std::endl << std::endl;
+            source << "namespace astlib {" << std::endl << std::endl;
 
-            source << "const std::string " << upperName << "_SYMBOL = \"" << entry.first << "\";" << std::endl;
+            for (const auto& entry : globals)
+            {
+                std::string upperName = Poco::toUpper(entry.first);
+                Poco::replaceInPlace(upperName, ".", "_");
+
+                source << "const std::string " << upperName << "_SYMBOL = \"" << entry.first << "\";" << std::endl;
+            }
+
+            source << std::endl << std::endl;
+            source << "static struct AsterixGeneratedEntry { const std::string& name; const std::string description; } codeToNameTable[ASTERIX_ITEM_COUNT] = {\n";
+
+            for (const auto& entry : globals)
+            {
+                std::string upperName = Poco::toUpper(entry.first);
+                Poco::replaceInPlace(upperName, ".", "_");
+
+                source << "    { " << upperName << "_SYMBOL, \"\" }," << std::endl;
+            }
+            source << "};" << std::endl << std::endl;
+
+            source << "static std::unordered_map<std::string, AsterixItemCode> symbolToCodeMap = {" << std::endl;
+            for (const auto& entry : globals)
+            {
+                std::string upperName = Poco::toUpper(entry.first);
+                Poco::replaceInPlace(upperName, ".", "_");
+
+                source << "    { " << upperName << "_SYMBOL, " << upperName << "_CODE }," << std::endl;
+            }
+            source << "};" << std::endl << std::endl;
+
+            const char functions[] = "AsterixItemCode asterixSymbolToCode(const std::string& symbol)\n"
+                "{\n"
+                "    return symbolToCodeMap[symbol];\n"
+                "}\n"
+                "\n"
+                "const std::string& asterixCodeToSymbol(AsterixItemCode code)\n"
+                "{\n"
+                "    int index = code.code();\n"
+                "    poco_assert(index < ASTERIX_ITEM_COUNT);\n"
+                "    return codeToNameTable[index+1].name;\n"
+                "}\n";
+
+            source << functions << std::endl;
+
+            source << "}" << std::endl;
         }
 
-        source << std::endl << std::endl;
-        source << "static struct AsterixGeneratedEntry { const std::string& name; const std::string description; } codeToNameTable[ASTERIX_ITEM_COUNT] = {\n";
+        // -------------------------------------------------------------------------------------------------------------
 
-        for (const auto& entry : globals)
         {
-            std::string upperName = Poco::toUpper(entry.first);
-            Poco::replaceInPlace(upperName, ".", "_");
+            Poco::FileOutputStream js("asterixitems.js");
+            js << "/// @brief file generated from XML asterix descriptions" << std::endl << std::endl;
 
-            source << "    { " << upperName << "_SYMBOL, \"\" }," << std::endl;
+            int index = 1;
+            for (const auto& entry : globals)
+            {
+                astlib::PrimitiveItem item = entry.second;
+                std::string upperName = Poco::toUpper(entry.first);
+                Poco::replaceInPlace(upperName, ".", "_");
+
+                astlib::AsterixItemCode code(index, item.getType().toValue(), item.isArray());
+                js << "const " << upperName << " = " << Poco::NumberFormatter::formatHex(code.value, 4, true) << ";  ///< " << item.getDescription() << std::endl;
+                index++;
+            }
+
+            js << std::endl;
         }
-        source << "};" << std::endl << std::endl;
 
-        source << "static std::unordered_map<std::string, AsterixItemCode> symbolToCodeMap = {" << std::endl;
-        for (const auto& entry : globals)
-        {
-            std::string upperName = Poco::toUpper(entry.first);
-            Poco::replaceInPlace(upperName, ".", "_");
-
-            source << "    { " << upperName << "_SYMBOL, " << upperName << "_CODE }," << std::endl;
-        }
-        source << "};" << std::endl << std::endl;
-
-        const char functions[] = "AsterixItemCode asterixSymbolToCode(const std::string& symbol)\n"
-            "{\n"
-            "    return symbolToCodeMap[symbol];\n"
-            "}\n"
-            "\n"
-            "const std::string& asterixCodeToSymbol(AsterixItemCode code)\n"
-            "{\n"
-            "    int index = code.code();\n"
-            "    poco_assert(index < ASTERIX_ITEM_COUNT);\n"
-            "    return codeToNameTable[index+1].name;\n"
-            "}\n";
-
-        source << functions << std::endl;
-
-        source << "}" << std::endl;
+        Poco::File jsFile("asterixitems.js");
+        jsFile.moveTo("../../node");
     }
     catch(Poco::Exception& e)
     {
